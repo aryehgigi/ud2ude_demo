1883ebcc05015b8872bc7f24a7ac15bc925d7bf0
aryehgigi <aryehgigi@gmail.com>
2019-12-26 20:47:16 +0200

    spaces bug fix

--- eud/compare_ud_types.py
+++ eud/compare_ud_types.py
@@ -2,12 +2,9 @@ import os
 from bottle import route, run, request, static_file
 import json
 import spacy
-#from spacy_conll import Spacy2ConllParser
-from ud2ude_aryehgigi.converter import convert, ConvsCanceler
-import ud2ude_aryehgigi.conllu_wrapper as cw
-
-#ARBITRARY_PATH = "sentence.txt"
-
+from spacy.tokens import Doc
+from ud2ude.converter import convert, ConvsCanceler
+import ud2ude.conllu_wrapper as cw
 
 @route('/eud/')
 @route('/eud/<filepath:path>')
@@ -16,7 +13,6 @@ def server_static(filepath="index.html"):
     if (" " in lastpart) or ("." not in lastpart):
         filepath = filepath.replace(lastpart, "index.html")
     return static_file(filepath, root='./')
-
 
 @route('/eud/annotate/', method='POST')
 def annotate():
@@ -31,27 +27,23 @@ def annotate():
     remove_eud_info = request.json["remove_eud_info"]
     remove_extra_info = request.json["remove_extra_info"]
     
-    # spacyconll.parseprint(input_str=sentence, output_file=ARBITRARY_PATH, is_tokenized=True)
-    # with open(ARBITRARY_PATH, "r") as f:
-    #     conllu_basic_out = f.read()
-    # os.remove(ARBITRARY_PATH)
-    
-    conllu_basic_out_formatted = cw.parse_spacy_doc(nlp(sentence))
+    doc = Doc(nlp.vocab, words=[t.text for t in nlp(sentence) if not t.is_space])
+    _ = tagger(doc)
+    _ = parser(doc)
+    conllu_basic_out_formatted = cw.parse_spacy_doc(doc)
     odin_basic_out = cw.conllu_to_odin([conllu_basic_out_formatted], is_basic=True, push_new_to_end=False)
     
-    conllu_plus_out_formatted, conv_done = convert([conllu_basic_out_formatted], eud, eud_pp, eud_aryeh, int(conv_iterations), remove_eud_info, remove_extra_info, False, ConvsCanceler())
+    conllu_plus_out_formatted, conv_done = convert([conllu_basic_out_formatted], eud, eud_pp, eud_aryeh, int(conv_iterations), remove_eud_info,
+                                                   remove_extra_info, False, ConvsCanceler())
     odin_plus_out = cw.conllu_to_odin(conllu_plus_out_formatted, push_new_to_end=False)
-
+    
     return json.dumps({
         "basic": odin_basic_out,
         "plus": odin_plus_out,
         "conv_done": conv_done,
     })
 
-
-# TODO:
-#   1. add to a main function
-#   2. copy the model to the project and remove the absolute local path
 nlp = spacy.load("en_ud_model")
-#spacyconll = Spacy2ConllParser(nlp=nlp)
 run(host='localhost', reloader=True, port=5070)
+tagger = nlp.get_pipe('tagger')
+parser = nlp.get_pipe('parser')
