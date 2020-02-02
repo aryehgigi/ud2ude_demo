89e8c2b9f6eebc4b76ac0270931070ce9ff59376
aryehgigi <aryehgigi@gmail.com>
2020-02-02 14:50:56 +0200

    change to BART

--- eud/compare_ud_types.py
+++ eud/compare_ud_types.py
@@ -22,10 +22,10 @@ def annotate():
     sentence = request.json["sentence"]
     eud = request.json["eud"]
     eud_pp = request.json["eud_pp"]
-    eud_aryeh = request.json["eud_aryeh"]
+    eud_bart = request.json["eud_bart"]
     conv_iterations = request.json["conv_iterations"]
     remove_eud_info = request.json["remove_eud_info"]
-    remove_extra_info = request.json["remove_extra_info"]
+    include_bart_info = request.json["include_bart_info"]
     
     doc = Doc(nlp.vocab, words=[t.text for t in nlp(sentence) if not t.is_space])
     _ = tagger(doc)
@@ -33,8 +33,8 @@ def annotate():
     conllu_basic_out_formatted = cw.parse_spacy_doc(doc)
     odin_basic_out = cw.conllu_to_odin([conllu_basic_out_formatted], is_basic=True, push_new_to_end=False)
     
-    conllu_plus_out_formatted, conv_done = convert([conllu_basic_out_formatted], eud, eud_pp, eud_aryeh, int(conv_iterations), remove_eud_info,
-                                                   remove_extra_info, False, ConvsCanceler())
+    conllu_plus_out_formatted, conv_done = convert([conllu_basic_out_formatted], eud, eud_pp, eud_bart, int(conv_iterations), remove_eud_info,
+                                                   not include_bart_info, False, ConvsCanceler())
     odin_plus_out = cw.conllu_to_odin(conllu_plus_out_formatted, push_new_to_end=False)
     
     return json.dumps({
